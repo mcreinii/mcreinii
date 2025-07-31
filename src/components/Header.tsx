150d91c3bf44b1162d87d6008da5ab891d471cf3
mcreinii <mcreinii@gmail.com>
2025-07-31 13:53:21 +0800

    Tinamad

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,16 +3,16 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header>
+    <header className="shadow-gray-500 shadow-md h-15 flex ">
       <Image
         src={
           'https://static.vecteezy.com/system/resources/thumbnails/001/840/612/small/picture-profile-icon-male-icon-human-or-people-sign-and-symbol-free-vector.jpg'
         }
-        width={200}
-        height={200}
+        width={40}
+        height={40}
         alt="Profile"
       />
-      <ul>
+      <ul className="self-center-safe">
         <li>
           <li>
             <Link href={'/'}>Home</Link>
